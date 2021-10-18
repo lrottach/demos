fde9c23f4ab0b4718b0df5e06c177f7bf3f92e08
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2021-10-18 16:36:35 +0200

    feat: set tag for existing resource

--- 2021-10-18_iac_demo/examples/storage.ps1
+++ 2021-10-18_iac_demo/examples/storage.ps1
@@ -1,5 +1,5 @@
 $location = "Switzerland North"
-$resourceGroup = "pdco-sto1-csn"
+$resourceGroup = "pdco-demo1-csn"
 $accountName = "pdcosto33csn"
 $tags = @{"Creator" = "lrottach@baggenstos.ch"; "Creation Date" = "18.10.2021" }
 
@@ -11,6 +11,11 @@ New-AzStorageAccount -ResourceGroupName $resourceGroup `
 	-Kind StorageV2 `
 	-Tags $tags
 
+# Add new tag to existing storage account
 $newTag = @{"Environment" = "Production" }
 
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroup -StorageAccount $accountName
+
+Set-AzResource -ResourceId $storageAccount.Id `
+	-Tag $newTag `
+	-Force
